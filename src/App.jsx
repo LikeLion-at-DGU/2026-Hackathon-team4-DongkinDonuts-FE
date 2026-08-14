f9147288df1b7d3782977622678c9b1f8d9f54cb
rhtjdco <gsc6690@gmail.com>
2026-08-15 00:42:46 +0900

    Feat: App 구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import AppRouter from "./router/AppRouter"
-import GlobalStyle from "./styles/GlobalStyle";
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { GlobalStyle } from "./styles/GlobalStyle";
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Layout = () => {
+  return (
+    <Wrapper>
+      <Header />
+      <Outlet />
+      <Footer />
+    </Wrapper>
+  );
+};
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <AppRouter />
+      <Layout />
     </>
   );
 }
