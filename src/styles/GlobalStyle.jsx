1147dc7acab7be5965c09e5b03381599a489f61b
rhtjdco <gsc6690@gmail.com>
2026-08-20 17:15:47 +0900

    Design: 폰트페밀리 지정

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,6 +1,104 @@
 import { createGlobalStyle } from "styled-components";
+import SuitThinWOFF2 from "../assets/fonts/SUIT-Thin.woff2";
+import SuitExtraLightWOFF2 from "../assets/fonts/SUIT-ExtraLight.woff2";
+import SuitLightWOFF2 from "../assets/fonts/SUIT-Light.woff2";
+import SuitRegularWOFF2 from "../assets/fonts/SUIT-Regular.woff2";
+import SuitMediumWOFF2 from "../assets/fonts/SUIT-Medium.woff2";
+import SuitSemiBoldWOFF2 from "../assets/fonts/SUIT-SemiBold.woff2";
+import SuitBoldWOFF2 from "../assets/fonts/SUIT-Bold.woff2";
+import SuitExtraBoldWOFF2 from "../assets/fonts/SUIT-ExtraBold.woff2";
+import SuitHeavyWOFF2 from "../assets/fonts/SUIT-Heavy.woff2";
+
 
 export const GlobalStyle = createGlobalStyle`
+/* ---------- Font Face Declarations ---------- */
+@font-face {
+  font-family: 'Pretendard';
+  src: url('/fonts/Pretendard-Regular.woff2') format('woff2');
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Pretendard';
+  src: url('/fonts/Pretendard-Bold.woff2') format('woff2');
+  font-weight: 700;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'SUIT';
+  src: url(${SuitThinWOFF2}) format('woff2');
+  font-weight: 100;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'SUIT';
+  src: url(${SuitExtraLightWOFF2}) format('woff2');
+  font-weight: 200;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'SUIT';
+  src: url(${SuitLightWOFF2}) format('woff2');
+  font-weight: 300;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'SUIT';
+  src: url(${SuitRegularWOFF2}) format('woff2');
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'SUIT';
+  src: url(${SuitMediumWOFF2}) format('woff2');
+  font-weight: 500;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'SUIT';
+  src: url(${SuitSemiBoldWOFF2}) format('woff2');
+  font-weight: 600;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'SUIT';
+  src: url(${SuitBoldWOFF2}) format('woff2');
+  font-weight: 700;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'SUIT';
+  src: url(${SuitExtraBoldWOFF2}) format('woff2');
+  font-weight: 800;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'SUIT';
+  src: url(${SuitHeavyWOFF2}) format('woff2');
+  font-weight: 900;
+  font-style: normal;
+  font-display: swap;
+}
 /* ---------- CSS Reset ---------- */
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
@@ -54,5 +152,11 @@ table {
 
 * {
   box-sizing: border-box;
+}
+
+body {
+  font-family: 'SUIT', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 `;
